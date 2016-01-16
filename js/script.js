98d03807651f499aeb52bda36386de5ade436c07
Vu Nguyen Hung <spamspam252@gmail.com>
2016-01-16 15:59:00 +0700

    Done submitButton. TODO: jQuery

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
 $(function() {
-  $( ".submit" ).click(function() {
-    $( ".submit" ).addClass( "onclic", 250, validate);
-  });
+    $("#submitMessage").click(function() {
+        $("#submitMessage").addClass("onclic", 250, validate);
+    });
 
-  function validate() {
-    setTimeout(function() {
-      $( ".submit" ).removeClass( "onclic" );
-      $( ".submit" ).addClass( "validate", 450, callback );
-    }, 2250 );
-  }
-    function callback() {
-      setTimeout(function() {
-        $( ".submit" ).removeClass( "validate" );
-      }, 1250 );
+    function validate() {
+        // window.alert("timeOut");
+        setTimeout(function() {
+            $("#submitMessage").removeClass("onclic");
+            $("#submitMessage").addClass("validate", 450, callback);
+        }, 2250);
     }
-  });
+
+    function callback(e) {
+        // window.alert("callback");
+        // setTimeout(function() {
+        //   $( "#submitMessage" ).removeClass( "validate" );
+        // }, 1250 );
+        e.disable();
+    }
+
+    $("#submitNewsLetter").click(function() {
+        $("#submitNewsLetter").addClass("onclic", 250, validate2);
+    });
+
+    function validate2() {
+        // window.alert("timeOut");
+        setTimeout(function() {
+            $("#submitNewsLetter").removeClass("onclic");
+            $("#submitNewsLetter").addClass("validate", 450, callback);
+        }, 2250);
+    }
+
+
+
+});
