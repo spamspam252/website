023fe6d3d9ca6dfa3c3fb13d3c0aad9458d64c02
Vu Nguyen Hung <spamspam252@gmail.com>
2016-01-18 20:22:14 +0700

    DONE: Logo + NavBar (jQuery)| NEXT: NavBar hover and sectionHeader pic

--- js/script.js
+++ js/script.js
@@ -1,28 +1,35 @@
 $(function() {
 
-    $('#name').hide();
+    $('#logo, #name, #About, #Portfolio, #Blog, #Contact').click(function(event) {
+        if (event.target.id == "logo" || event.target.id == "name")
+            $(window).scrollTo('0%', 500);
+        else if (event.target.id == "About")
+            $(window).scrollTo('24.9%', 500);
+        else if (event.target.id == "Portfolio")
+            $(window).scrollTo('40.3%', 500);
+        else if (event.target.id == "Blog")
+            $(window).scrollTo('60.1%', 500);
+        else if(event.target.id == "Contact")
+            $(window).scrollTo('89.3%', 500);
+
+    });
+
+    // $(window).scrollTo('0%', 500
+    // // , {  easing: 'easeInOutQuad'}
+    // );
+
+
     var lastScrollTop = 0;
     $(window).scroll(function(event) {
         var st = $(this).scrollTop();
-        if (st > lastScrollTop) {
-            // downscroll code
-            // console.log("down");
-            // $('#logo').slideUp('fast');
-            // $('#name').slideDown('fast');
-            // $('#FixedMenu').css({
-            //     height: '90px'
-            // });
+        if (st >= lastScrollTop) {
+
+            $('#FixedMenu').addClass("menuCollapse");
 
         } else {
             // upscroll code
-            if (st === 0) {
-                // $('#logo').slideDown('fast');
-                // $('#name').slideUp('fast').fadeOut('fast', function() {
-
-                // });;
-                // $('#FixedMenu').css({
-                //     height: '110px'
-                // });
+            if (st <= 0) {
+                $('#FixedMenu').removeClass('menuCollapse');
             }
         }
         lastScrollTop = st;
